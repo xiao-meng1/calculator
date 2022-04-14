33048c77b44b4b5192eb6e5837ce8e8c883302c7
Xiao <xmeng1@ualberta.ca>
2022-04-14 11:58:22 -0600

    connect button and operator logic

--- app.js
+++ app.js
@@ -1,41 +1,70 @@
-addButtonEvents();
+handleButtons();
 
+function handleButtons() {
+    addButtonEvents();
 
-function addButtonEvents() {
-    const numberButtons = document.querySelectorAll(".number-button");
-    numberButtons.forEach((button) => {
-        button.addEventListener("click", numberButtonClick);
-    });
+    let num1 = "";
+    let num2 = "";
+    let operator = "";
 
-    const operatorButton = document.querySelectorAll(".operator-button");
-    operatorButton.forEach((button) => {
-        button.addEventListener("click", operatorButtonClick);
-    });
+    function addButtonEvents() {
+        const numberButtons = document.querySelectorAll(".number-button");
+        numberButtons.forEach((button) => {
+            button.addEventListener("click", numberButtonClick);
+        });
+
+        const operatorButton = document.querySelectorAll(".operator-button");
+        operatorButton.forEach((button) => {
+            button.addEventListener("click", operatorButtonClick);
+        });
+
+        const equalsButton = document.querySelector("#equals-button");
+        equalsButton.addEventListener("click", equalsButtonClick);
+    }
+
+    function numberButtonClick(e) {
+        display = document.querySelector("#display");
+        display.innerText += e.target.innerText;
+    }
+
+    function operatorButtonClick(e) {
+        display = document.querySelector("#display");
+        
+        if (operator !== "") {
+            equalsButtonClick();
+            return;
+        }
+
+        num1 = display.innerText;
+        operator = e.target.innerText;
+        display.innerText += e.target.innerText;
+    }
+
+    function equalsButtonClick() {
+        display = document.querySelector("#display");
+        num2 = display.innerText.slice(num1.concat(operator).length);
+        display.innerText = operate(operator, num1, num2);
+
+        num1 = "";
+        num2 = "";
+        operator = "";
+    }
 }
 
-function numberButtonClick(e) {
-    display = document.querySelector("#display");
-    display.innerText += e.target.innerText;
-}
-
-function operatorButtonClick(e) {
-    display = document.querySelector("#display");
-    display.innerText += e.target.innerText;
-}
 
 function operate(operator, num1, num2) {
     let result;
     switch (operator) {
-        case "add":
+        case "+":
             result = add(num1, num2);
             break;
-        case "subtract":
+        case "-":
             result = subtract(num1, num2);
             break;
-        case "multiply":
+        case "x":
             result = multiply(num1, num2);
             break;
-        case "divide":
+        case "%":
             result = divide(num1, num2);
             break;
     }
@@ -43,17 +72,17 @@ function operate(operator, num1, num2) {
 }
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
